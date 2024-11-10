8d45bd6c07d2ca8873370df4a9f607a78ae1c277
andrew-mattheus <117692544+andrew-mattheus@users.noreply.github.com>
2024-11-09 20:02:02 -0500

    Update rke2_config.sh

--- rke2_config.sh
+++ rke2_config.sh
@@ -1,4 +1,7 @@
 # rke2_config.sh
+
+# rke2_config.sh
 INSTALL_RKE2_TAR_PREFIX="/usr/local"
 INSTALL_RKE2_AGENT_IMAGES_DIR="/var/lib/rancher/rke2/agent/images"
-INSTALL_RKE2_ARTIFACT_PATH="/path/to/local/tarballs"  # Update with actual path
+INSTALL_RKE2_ARTIFACT_PATH="/path/to/local/tarballs"  # Path to local directory for tarballs
+RKE2_TARBALL_URL="https://github.com/rancher/rke2/releases/download/v1.30.6%2Brke2r1/rke2.linux-amd64.tar.gz"
