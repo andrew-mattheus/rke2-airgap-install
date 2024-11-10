a319e5c8bbd5baf5a19dadde3f611da8cbacff39
andrew-mattheus <117692544+andrew-mattheus@users.noreply.github.com>
2024-11-09 19:57:50 -0500

    Create rke2_config.sh

---
+++ rke2_config.sh
@@ -0,0 +1,4 @@
+# rke2_config.sh
+INSTALL_RKE2_TAR_PREFIX="/usr/local"
+INSTALL_RKE2_AGENT_IMAGES_DIR="/var/lib/rancher/rke2/agent/images"
+INSTALL_RKE2_ARTIFACT_PATH="/path/to/local/tarballs"  # Update with actual path
